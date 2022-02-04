d9e30097f68963f0c23debd1b1bfbe0e875e18f1
Mike Taylor <mike@whatsmycut.com>
2022-02-03 18:09:16 -0800

    Render method

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import client from './api/clients/apollo';
 import './App.css';
 import WMCPApp from './wmcpApp';
 import { ThemeProvider, Theme, StyledEngineProvider, createTheme } from '@mui/material/styles';
 import { ApolloProvider } from 'react-apollo';
+import ReactDOM from 'react-dom';
 
 declare module '@mui/styles/defaultTheme' {
   interface DefaultTheme extends Theme {}
 }
 const theme = createTheme();
 
-function App() {
-  return (
+export const App = () => {
+  const render = () => (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
           <ApolloProvider client={client}>
@@ -21,6 +22,14 @@ function App() {
       </ThemeProvider>
     </StyledEngineProvider>
   );
-}
 
+  if (module.hot) {
+    module.hot.accept('./wmcpApp', () => {
+      return render();
+    });
+  };
+  return render()
+
+}
+ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
